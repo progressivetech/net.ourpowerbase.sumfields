7929f7d52b686a8944fed4fb8a3bc28239bffd3b
Jamie McClelland <jm@mayfirst.org>
2013-01-30 13:00:46 -0500

    improving comments in the code.

--- cff.php
+++ cff.php
@@ -102,7 +102,7 @@ function cff_civicrm_triggerInfo(&$info, $tableName) {
     list($id, $$name_var) = $field_info;
   } 
 
-  // Mamba jamba sql trigger sql statement that hopefully won't
+  // Mamba jamba trigger sql statement that hopefully won't
   // be a performance killer.
   $sql = "REPLACE INTO `$calc_fr_table_name` SET 
     `$total_lifetime_field` = 
@@ -136,6 +136,7 @@ function cff_civicrm_triggerInfo(&$info, $tableName) {
     WHERE contact_id = NEW.contact_id AND contribution_status_id = 1), 
     entity_id = NEW.contact_id;";
 
+  // We want to fire this trigger on both insert and update.
   $info[] = array(
     'table' => 'civicrm_contribution',
     'when' => 'AFTER',
@@ -166,7 +167,9 @@ function _cff_generate_data_based_on_current_contributions() {
   // NOTE: MySQL ignores the AS spec - the rows are assigned based on order, not 
   // based on column name. I'm keeping the AS statements for reference so we know
   // which calcs are for which fields - however keep in mind, the column names may
-  // not properly match the actual column names (which will have IDs appended to them).
+  // not properly match the actual column names (which may have IDs appended to them).
+  // If we change the order in which the columns are created, we'll need to change
+  // the order here too.
   $sql = "INSERT INTO `$table_name` 
     SELECT
       NULL,
@@ -209,7 +212,7 @@ function _cff_create_custom_fields() {
 /**
  * Delete custom fields. All data should be 
  * generated data, so no worry about deleting
- * anything that should be kept
+ * anything that should be kept.
  **/
 function _cff_delete_custom_fields() {
   $session = CRM_Core_Session::singleton();
@@ -236,7 +239,7 @@ function _cff_delete_custom_fields() {
 
 /**
  * Create an array of fields and field definitions from
- * the xml file
+ * the xml file.
  **/
 
 function _cff_get_custom_field_definitions() {
