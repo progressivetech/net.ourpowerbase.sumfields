f4472b7e7f891b95ddfb4271bc99ce1da544e64a
Jamie McClelland <jm@mayfirst.org>
2019-02-05 11:14:45 -0500

    missed upgrader script in upgrade commit.

---
+++ CRM/Sumfields/Upgrader.php
@@ -0,0 +1,33 @@
+<?php
+use CRM_Sumfields_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Sumfields_Upgrader extends CRM_Sumfields_Upgrader_Base {
+
+  /**
+   * Re-build triggers and re-generate data.
+   *
+   * Since version 4.0.0 now tracks contributions by line items, we need
+   * to rebuild triggers and rebuild data to ensure we are current. 
+   *
+   * @return TRUE on success
+   * @throws Exception
+   **/
+  public function upgrade_4000() {
+    $this->ctx->log->info('Planning update 4000'); // PEAR Log interface
+    $this->addTask(E::ts('Regenerate Data'), 'regenerateData');
+    $this->addTask(E::ts('Rebuild Triggers'), 'triggerRebuild');
+    return TRUE;
+  }
+
+  public function rebuildTriggers() { 
+    CRM_Core_DAO::triggerRebuild();
+    return TRUE;
+  }
+
+  public function regenerateData() { 
+    return sumfields_generate_data_based_on_current_data();
+  }
+}
