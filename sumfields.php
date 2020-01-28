37dfe93bf54ae00fb6132def58605a4cacc6f960
Jamie McClelland <jm@mayfirst.org>
2020-01-28 12:03:36 -0500

    get order right on in_array call.

--- sumfields.php
+++ sumfields.php
@@ -325,7 +325,7 @@ function sumfields_civicrm_triggerInfo(&$info, $tableName) {
     // Iterate over all our fields, and build out a sql parts array
     foreach($custom_fields as $base_column_name => $params) {
       if(!in_array($base_column_name, $active_fields)) continue;
-      if(!in_array($custom['fields'], $base_column_name)) {
+      if(!in_array($base_column_name, $custom['fields'])) {
         Civi::log()->debug("The $base_column_name field has been yanked, re-save custom field definitions. ");
         continue;
       }
